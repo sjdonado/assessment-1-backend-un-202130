cc20a33ad1b0814252ddd2993785c77d8d7069b5
Rafael Velandia <ravelandia@uninorte.edu.co>
2021-08-14 00:26:31 +0000

    Se cometio un error en el main.yml, ademas el Fibonacci en bash sirve

--- parcial1/entrypoint.sh
+++ parcial1/entrypoint.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-N=$INPUT_N_INDEX
-echo -n "$N "
-
+# Program for Fibonacci
+# Series
+   
+# Static input fo N
+N=6
+  
+# First Number of the
+# Fibonacci Series
 a=0
-
+  
+# Second Number of the
+# Fibonacci Series
 b=1 
    
 echo "The Fibonacci series is : "
    
 for (( i=0; i<N; i++ ))
 do
-    #echo -n "$a "
+    echo -n "$a "
     fn=$((a + b))
     a=$b
     b=$fn
 done
-echo -n "$a "
+# End of for loop
