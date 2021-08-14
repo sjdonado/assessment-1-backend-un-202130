7af6b39c2c459acf0707eea8847192d2c076a987
Rafael Velandia <ravelandia@uninorte.edu.co>
2021-08-14 01:41:13 +0000

    Da la respuesta en bash y se elimino el default

--- parcial1/entrypoint.sh
+++ parcial1/entrypoint.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
-# rsync using variables
-# Program for Fibonacci
-# Series
-   
-# Static input fo N
-
 
 
 N=$INPUT_N_INDEX 
-  
-# First Number of the
-# Fibonacci Series
+
 a=1
-  
-# Second Number of the
-# Fibonacci Series
+
 b=1 
-   
-   
+ 
 for (( i=1; i<N; i++ ))
 do
     fn=$((a + b))
