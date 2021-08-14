e0330a7b87ce15d570e4e4ae4c5eb7e85a26f732
Rafael Velandia <ravelandia@uninorte.edu.co>
2021-08-14 00:20:12 +0000

    Prueba Fibonacci en bash

--- parcial1/entrypoint.sh
+++ parcial1/entrypoint.sh
@@ -1,2 +1,19 @@
-# !/bin/bash
-echo "Esto funciona Confirmado" 
+#!/bin/bash
+
+N=$INPUT_N_INDEX
+echo -n "$N "
+
+a=0
+
+b=1 
+   
+echo "The Fibonacci series is : "
+   
+for (( i=0; i<N; i++ ))
+do
+    #echo -n "$a "
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
+echo -n "$a "
