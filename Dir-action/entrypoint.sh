ae0c190fdb3358578230de26c63c1dbd80140918
kangcheng322 <leik@example.com>
2021-08-15 01:47:25 +0000

    ci(entrypoint.sh): Add fibonacci series program to file

--- Dir-action/entrypoint.sh
+++ Dir-action/entrypoint.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "Mi nombre es: $INPUT_N_INDEX" 
+echo "Input: $INPUT_N_INDEX" 
+echo "Output: "
+#Fibonacci Series
+
+#Input
+Number=$INPUT_N_INDEX
+
+#First Number of the Fibonacci Series
+a=1
+
+# Second Number of the Fibonacci Series
+b=1
+
+
+for (( i=2; i<=Number; i++ ))
+do
+	echo -n "$a "
+	fn=$((a + b))
+	a=$b
+	b=$fn
+done
+
+echo -e "\033[36m$a\033[0m"
