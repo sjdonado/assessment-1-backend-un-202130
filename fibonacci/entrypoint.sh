418733e4ba597016fa96df1cb3411c9b678478de
ricardo <ralvarezcm@gmail.com>
2021-08-15 18:03:43 -0500

    Hotfix#2 Dockerfile - fibonacci's github action

--- fibonacci/entrypoint.sh
+++ fibonacci/entrypoint.sh
@@ -2,4 +2,6 @@
 
 ## Podria haber puesto el entry point directo al script de python pero pienso que esto es lo que decia las instrucciones el examen.
 echo "Ejecutando secuencia de fibonacci con index $INPUT_N_INDEX"
+ls
+cd /
 python fib.py $INPUT_N_INDEX
