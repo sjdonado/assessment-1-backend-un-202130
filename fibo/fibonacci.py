49f7a0802f3f4eea2939294bbbda979e0da14c07
Juan Lagares <jdlagares@uninorte.edu.co>
2021-08-15 21:17:49 +0000

    build: se creo la carpeta fibo y cambios del workflow

---
+++ fibo/fibonacci.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+n=int(sys.argv[1])
+print(fibonacci((n)))
