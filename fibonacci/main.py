c308acb56296b074d3c61bdc29fac1e644f37087
sdariza <sdariza@uninorte.edu.co>
2021-08-16 16:51:53 +0000

    feat: Add fibonacci action + main workflow - (modified)

---
+++ fibonacci/main.py
@@ -0,0 +1,14 @@
+# Fibonacci Series using Dynamic Programming
+#Taken from https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
+import sys
+def fibonacci(n):
+     
+    # Taking 1st two fibonacci numbers as 0 and 1
+    f = [0, 1]
+     
+     
+    for i in range(2, n+1):
+        f.append(f[i-1] + f[i-2])
+    return f[n]
+     
+print(fibonacci(int(sys.argv[1])))
