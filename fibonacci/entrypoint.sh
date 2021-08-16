054276bb8bee1ca17f54a8b95d3b6843fe7554c7
Daporto <daporto@uninorte.edu.co>
2021-08-16 21:52:01 +0000

    feat: fibonacci script was added to entrypoint.sh file

--- fibonacci/entrypoint.sh
+++ fibonacci/entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "Hello world $INPUT_MY_NAME"
+fibonacci () {
+  if test $1 -le 1; then
+    return $2
+  else
+    fibonacci $(($1-1)) $(($2+$3)) $2
+  fi
+}
+
+fibonacci_output () {
+  fibonacci $1 1 0
+  echo $?
+}
+
+fibonacci_output $INPUT_N_INDEX
