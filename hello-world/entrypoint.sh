a259e68485084d6197c509f084b5516c506986cf
Ronald Carreno <ronaldoc@uninorte.edu.co>
2021-08-17 23:53:35 +0000

    add entrypoint.sh with fib algorithm in bash

--- hello-world/entrypoint.sh
+++ hello-world/entrypoint.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-sudo chmod +x Fibonacci
+# Program for Fibonacci
+# Series
+   
+# Static input fo N
+N=6
+  
+# First Number of the
+# Fibonacci Series
+a=0
+  
+# Second Number of the
+# Fibonacci Series
+b=1 
+   
+echo "The Fibonacci series is : "
+   
+for (( i=0; i<N; i++ ))
+do
+    echo -n "$a "
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
