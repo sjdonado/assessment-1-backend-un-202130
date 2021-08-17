6a9721ef6e861b982ed5293bbb9d4d19027f8a70
Andresifico <amezaf@uninorte.edu.co>
2021-08-17 01:04:50 +0000

    fix: error in fibonacci encountered and fixed

--- parcial_1/entrypoint.sh
+++ parcial_1/entrypoint.sh
@@ -2,18 +2,19 @@
 # Programa de Fibonacci
 
 # N
+
 N=$INPUT_N_INDEX 
 
 a=0
 b=1
+
 echo "El resultado Fibonacci es : "
 
-
-for (( i=0; i<N; i+ ))
+for (( i=0; i<N; i++ ))
 do
 	fn=$((a + b))
 	a=$b
-	b=$f
+	b=$fn
 done
 echo -n "$a "
 
