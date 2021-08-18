26d9d1fe31ca38dc066fab66b112cc0d5a75d25f
Daniel Marquez <dgmarquez@uninorte.edu.co>
2021-08-17 20:55:02 -0500

    2ndo Commit entrypoint.sh

--- hello-world/entrypoint.sh
+++ hello-world/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Hello world $INPUT_MY_NAME"
+echo "N_INDEX = $INPUT_N_INDEX"
+a=0
+b=1
+
+for (( i=0; i<$INPUT_N_INDEX; i++ ))
+do
+	fn=$((a + b))
+	a=$b
+	b=$fn
+done
+
+echo "Output: $a"
