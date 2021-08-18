ff5b924d5893e95c94dc9e4f67170a3af984b6bc
estnelsoncaraballo <88053996+estnelsoncaraballo@users.noreply.github.com>
2021-08-17 21:29:35 -0500

    fix: obtener argumentos

--- fibonacci.py
+++ fibonacci.py
@@ -8,7 +8,9 @@ def fib(n):
         return fib(n-1) + fib(n-2)
 list = sys.argv
 #if len(list) == 3:
-print(fib(9))
+print("El resultado es: " + str(fib(9)))
+print("Los argumentos son:")
+print(*list)
 #else:
 #  for i in range(len(list)):
 #    print(list[i])
