dbe9454918d4bb86a41102cb18ce4e1a5e6fe383
@ingnelsoncaraballo <encaraballo@uninorte.edu.co>
2021-08-17 23:37:11 -0500

    feat: se añadio code en sh

--- fibonacci/fibonacci.sh
+++ fibonacci/fibonacci.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
-# Function f calculates n'th Fibonacci number
-# It uses ALGORITHM 2B: SIMPLE RECURSION
-f () {
-  if test $1 -le 1; then
-    return $2
-  else
-    f $(($1-1)) $(($2+$3)) $2
-  fi
-}
-# Function f_print prints out n'th Fibonacci number
-f_print () {
-  f $1 1 1
-  echo $1 "th Fibonacci number is" $?
-}
-f_print 9
+# !/bin/bash
+
+N=$INPUT_N_INPUT
+a=0
+b=1
+ 
+echo "La serie de fibonacci es: "
+ 
+for (( i=0; i<=N; i++ ))
+do
+    if [[ i -eq N ]]
+    then
+        echo -n "$a "
+    fi
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
