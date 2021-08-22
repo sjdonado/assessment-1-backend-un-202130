eaf2c5b2076de574e101adb988ce29500f6487b6
jcarteaga1 <arteagajr07@gmail.com>
2021-08-21 22:38:26 -0500

    fixes

--- exam/entrypoint.sh
+++ exam/entrypoint.sh
@@ -2,12 +2,17 @@
 
 echo " N_INDEX $INPUT_N_INDEX"
 
-n = $INPUT_N_INDEX
-fn = 0
+n=$INPUT_N_INDEX
+base=0
+base_1=1
+fb=0
 
-for ((i = 0; i < n; i++)); 
+for ((i = 0; i <n-1; i++)); 
 do
-    fn=$(( i + (i + 1 ) ))
+    fb=$((base + base_1))
+    echo "$fb"
+    base=$base_1
+    base_1=$fb
 done
 
-echo "Fibonacci: $fn"
+echo "Fibonacci: $fb"
